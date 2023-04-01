ca272e6e6d4b9dc223e0513473df2df8d4881f3f
Manoj M <manoj.mb.murali@gmail.com>
2023-04-01 19:09:57 +0530

    added till publisher subscriber concept

---
+++ pub_sub_py/pub_sub_py/shop.py
@@ -0,0 +1,27 @@
+import rclpy 
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class GetItemsFromFarmers(Node):
+    
+    def __init__(self):
+        super().__init__("shop")
+
+        self.shop_in = self.create_subscription(String,"/farmer1_items",self.getitem_callback,10)
+
+        self.shop_out = self.create_publisher(String,"/apple")
+
+
+
+    def getitem_callback(self,msg: String):
+        self.get_logger().info(str(msg.data)+" is received")
+
+def main(args=None):
+    rclpy.init(args=args)
+    shop = GetItemsFromFarmers()
+    rclpy.spin(shop)
+    shop.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
