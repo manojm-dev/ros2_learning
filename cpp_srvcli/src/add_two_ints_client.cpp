85771ed5e31f62ce4e8fd409a04524d59d93f82e
Manoj M <manojmdev@proton.me>
2024-05-23 06:59:27 +0000

    added cpp srv cli example

--- cpp_srvcli/src/add_two_ints_client.cpp
+++ cpp_srvcli/src/add_two_ints_client.cpp
@@ -34,11 +34,11 @@ int main(int argc, char **argv)
 
   auto result = client->async_send_request(request);
   // Wait for the result.
-  if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
+  if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
-  } else {
+  } 
+  else {
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
   }
 
